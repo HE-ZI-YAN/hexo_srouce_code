9ba5bcb297cb9f109d2be8bcd1a31e79455a7d7a
HE-ZI-YAN <2571287622@qq.com>
2024-05-03 21:10:46 +0800

    添加评论功能

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 mkdir themes
 git clone https://github.com/blinkfox/hexo-theme-matery themes/hexo-theme-matery
 cp ./_config.theme.yml ./themes/hexo-theme-matery/_config.yml
-#cp -r ./asserts/images/* ./themes/Anatolo/source/images/
+
+cp -r ./asserts/contact.ejs ./themes/hexo-theme-matery/layout/contact.ejs
+cp -r ./asserts/post-detail.ejs ./themes/hexo-theme-matery/layout/_partial/post-detail.ejs
+cp -r ./asserts/utteranc.ejs ./themes/hexo-theme-matery/layout/_partial/utteranc.ejs
 
 npm install hexo-renderer-pug --save
 npm install hexo-renderer-stylus --save
--- source/diary/index.md
+++
@@ -1,6 +0,0 @@
----
-title: diary
-date: 2018-09-30 17:25:30
-type: "diary"
-layout: "diary"
----
