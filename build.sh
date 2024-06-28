ca19e8a4e509f3d50ba3d37b2787cbc3ea91873c
HE-ZI-YAN <2571287622@qq.com>
2024-06-28 14:52:48 +0800

    美化样式+1

--- build.sh
+++ build.sh
@@ -7,7 +7,15 @@ cp -r ./asserts/post-detail.ejs ./themes/hexo-theme-matery/layout/_partial/post-
 cp -r ./asserts/utteranc.ejs ./themes/hexo-theme-matery/layout/_partial/utteranc.ejs
 cp -r ./asserts/404.ejs ./themes/hexo-theme-matery/layout/404.ejs
 cp -r ./asserts/_partial/bg-cover-content.ejs ./themes/hexo-theme-matery/layout/_partial/bg-cover-content.ejs
-cp -r ./asserts/source ./themes/hexo-theme-matery/source
+cp -r ./asserts/source/favicon.png ./themes/hexo-theme-matery/source/favicon.png
+cp -r ./asserts/source/medias/logo.png ./themes/hexo-theme-matery/source/medias/logo.png
+
+cp -r ./asserts/source/medias/reward/wechat.png ./themes/hexo-theme-matery/source/medias/reward/wechat.png
+cp -r ./asserts/source/medias/reward/alipay.jpg ./themes/hexo-theme-matery/source/medias/reward/alipay.jpg
+
+cp -r ./asserts/source/libs/ ./themes/hexo-theme-matery/source/libs/
+
+cp -r ./asserts/source/css/matery.css ./themes/hexo-theme-matery/source/css/matery.css
 
 npm install hexo-renderer-pug --save
 npm install hexo-renderer-stylus --save
