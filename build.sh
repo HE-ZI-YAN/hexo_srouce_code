f869cd2065151f87b11c5b9e531f64f81d3780a3
HE-ZI-YAN <2571287622@qq.com>
2024-06-28 13:37:55 +0800

    美化样式

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ cp ./_config.theme.yml ./themes/hexo-theme-matery/_config.yml
 cp -r ./asserts/contact.ejs ./themes/hexo-theme-matery/layout/contact.ejs
 cp -r ./asserts/post-detail.ejs ./themes/hexo-theme-matery/layout/_partial/post-detail.ejs
 cp -r ./asserts/utteranc.ejs ./themes/hexo-theme-matery/layout/_partial/utteranc.ejs
+cp -r ./asserts/404.ejs ./themes/hexo-theme-matery/layout/404.ejs
+cp -r ./asserts/_partial/bg-cover-content.ejs ./themes/hexo-theme-matery/layout/_partial/bg-cover-content.ejs
 
 npm install hexo-renderer-pug --save
 npm install hexo-renderer-stylus --save
--- source/about/index.md
+++ source/about/index.md
@@ -5,3 +5,4 @@ type: "about"
 layout: "about"
 ---
 
+这是关于我的一些内容
